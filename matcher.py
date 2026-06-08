9cfbe8a34488e003912ddc272a5d630f7d433cc8
panigrahishruti2580-max <panigrahishruti2580@gmail.com>
2026-06-08 21:12:00 +0530

    Add files via upload

---
+++ matcher.py
@@ -0,0 +1,31 @@
+def calculate_match_score(
+    resume_skills,
+    jd_skills
+):
+
+    if len(jd_skills) == 0:
+        return 0
+
+    matched = set(
+        resume_skills
+    ).intersection(
+        set(jd_skills)
+    )
+
+    score = (
+        len(matched)
+        / len(jd_skills)
+    ) * 100
+
+    return round(score, 2)
+
+
+def missing_skills(
+    resume_skills,
+    jd_skills
+):
+
+    return list(
+        set(jd_skills)
+        - set(resume_skills)
+    )
